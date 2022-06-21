414c08d292358dfc8f50f26d32a3b66d731aac28
FabianFle <fabianflegler1@yahoo.de>
2022-06-21 22:05:13 +0200

    added more features

---
+++ database.js
@@ -0,0 +1,219 @@
+let questions = [
+
+    { //HTML
+
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bierber",
+        "right_answer": 3
+    },
+    {
+        "question": "Wie fügt man ein Bild ein?",
+        "answer_1": "<picture='...'>",
+        "answer_2": "<body img='...' alt='...'>",
+        "answer_3": "<href src='...' alt='...'>",
+        "answer_4": "<img src='...' alt='...''>",
+        "right_answer": 4
+    },
+    {
+        "question": "Mit welchem HTML-Element wird ein Text kursiv geschrieben?",
+        "answer_1": "< k >",
+        "answer_2": "< p >",
+        "answer_3": "< u >",
+        "answer_4": "< i >",
+        "right_answer": 4
+    },
+    {
+        "question": "Welche Aussage stimmt nicht?",
+        "answer_1": "HTML-Dateien können in jedem Browser geöffnet werden",
+        "answer_2": "HTML-Dateien können mit jedem beliebigen Editor erstellt werden",
+        "answer_3": "HTML ist eine Programmiersprache",
+        "answer_4": "HTML ist für Webseiten",
+        "right_answer": 3
+    },
+
+    //########################################
+
+    {//CSS
+
+        "question": "Was ist CSS?",
+        "answer_1": "Eine Programmiersprache",
+        "answer_2": "Eine Computersprache um Websites aufzubauen und zu strukturieren",
+        "answer_3": "Eine JS funktion",
+        "answer_4": " Weiß ich nicht",
+        "right_answer": 2
+    },
+    {
+        "question": "Wie werden Kommentare in CSS hinzugefügt?",
+        "answer_1": "/* das ist ein Kommentar */",
+        "answer_2": "// das ist ein Kommentar //",
+        "answer_3": "// das ist ein Kommentar",
+        "answer_4": "<!-- das ist ein Kommentar-->",
+        "right_answer": 1
+    },
+    {
+        "question": "Mit welcher Eigenschaft wird die Textfarbe verändert?",
+        "answer_1": "fontcolor:",
+        "answer_2": "font-color:",
+        "answer_3": "color:",
+        "answer_4": "textcolor:",
+        "right_answer": 3
+    },
+    {
+        "question": "Was ist der Inhalt eines Stylesheets?",
+        "answer_1": "Ersatzangaben für die font-Tags",
+        "answer_2": "Formatangaben zur Darstellung eines Webdokuments",
+        "answer_3": "Angaben, die verschiedene Inhalte der Website miteinander verbinden",
+        "answer_4": "Struktur der Website",
+        "right_answer": 2
+    },
+
+    //########################################
+
+
+    {//JS
+
+        "question": "Wie heißt der Erfinder von JavaScript?",
+        "answer_1": "Douglas Crockford",
+        "answer_2": "John Resig",
+        "answer_3": "Brendan Eich",
+        "answer_4": "Chuck Norris",
+        "right_answer": 3
+    },
+    {
+        "question": "JavaScript ist ein/e....?",
+        "answer_1": "Freamwork",
+        "answer_2": "Programmiersprache",
+        "answer_3": "Desinsprache",
+        "answer_4": "CMD Sprache",
+        "right_answer": 2
+    },
+    {
+        "question": "Wo schreibt man den JavaScript code rein?",
+        "answer_1": "<header></header>",
+        "answer_2": "<style></style>",
+        "answer_3": "<script></script>",
+        "answer_4": "<body></body>",
+        "right_answer": 3
+    },
+    {
+        "question": "Worum handelt es sich hierbei: for (let i = 0; i < 0; i++) {}?",
+        "answer_1": "Array",
+        "answer_2": "Funktion",
+        "answer_3": "String",
+        "answer_4": "For-Schleife",
+        "right_answer": 4
+    },
+
+    //########################################
+
+    {//Galaxy
+
+        "question": "Wie lange dauert es, bis man eine veränderung der Sonne, auf der Erde sieht?",
+        "answer_1": "1 Stunde",
+        "answer_2": "8 Minuten",
+        "answer_3": "Sofort",
+        "answer_4": "20 Minuten",
+        "right_answer": 2
+},
+    {
+        "question": "Wie viele Planeten hat unsere Sonnensystem?",
+        "answer_1": "6",
+        "answer_2": "8",
+        "answer_3": "10",
+        "answer_4": "12",
+        "right_answer": 2
+},
+    {
+        "question": "Wie groß ist die Erde?",
+        "answer_1": "5.349 km",
+        "answer_2": "7.432 km",
+        "answer_3": "6.334 km",
+        "answer_4": "6.371 km",
+        "right_answer": 4
+},
+    {
+        "question": "An welcher stelle steht unsere Erde im Sonnensystem?",
+        "answer_1": "3",
+        "answer_2": "4",
+        "answer_3": "5",
+        "answer_4": "6",
+        "right_answer": 1
+},
+
+    //########################################
+
+    {//Tierwelt
+
+        "question": "Was ist das größte Tier der welt?",
+        "answer_1": "Potwal",
+        "answer_2": "Blauwal",
+        "answer_3": "Weißer Hai",
+        "answer_4": "Orkar",
+        "right_answer": 2
+},
+    {
+        "question": "Wie schwer kann ein Blauwal werden?",
+        "answer_1": "Bis zu 1.000 Kilogramm",
+        "answer_2": "Bis zu 20.000 Kilogramm",
+        "answer_3": "Bis zu 95.000 Kilogramm",
+        "answer_4": "Bis zu 200.000 Kilogramm",
+        "right_answer": 4
+},
+    {
+        "question": "Welches Tier nennen wir auch = den besten Freund des Menschen?",
+        "answer_1": "Löwe",
+        "answer_2": "Spatz",
+        "answer_3": "Hund",
+        "answer_4": "Delfin",
+        "right_answer": 3
+},
+    {
+        "question": "Zu welcher Familie gehört der Fuchs?",
+        "answer_1": "Zur Familie der Katzen",
+        "answer_2": "Zur Familie der Hunde",
+        "answer_3": "Zur Familie der Bären",
+        "answer_4": "Zur Familie der Mäuse",
+        "right_answer": 2
+},
+
+    //########################################
+
+    {//Technik
+
+        "question": "Welche Reihenfolge ist richtig",
+        "answer_1": "byte - Kilobyte - Megabyte - Gigabyte - Terabyte - Petabyte",
+        "answer_2": "byte - Megabyte - Kilobyte - Gigabyte - Terabyte - Petabyte",
+        "answer_3": "Gigabyte - byte - Megabyte - Kilobyte - Terabyte - Petabyte",
+        "answer_4": "byte - Kilobyte - Gigabyte - Megabyte - Terabyte - Petabyte",
+        "right_answer": 1
+    },
+    {
+        "question": "Was ist keine Leistungssteigerer komponente",
+        "answer_1": "GPU",
+        "answer_2": "CPU",
+        "answer_3": "RAM",
+        "answer_4": "Mainboard",
+        "right_answer": 4
+    },
+    {
+        "question": "Was heißt CPU",
+        "answer_1": "Core Programmable User",
+        "answer_2": "Calculating Professor Unit",
+        "answer_3": "Central Processing Unit",
+        "answer_4": "County Pushchair Unveiling",
+        "right_answer": 3
+    },
+    {
+        "question": "Was verbirgt sich hinter der Abkürzung SATA?",
+        "answer_1": "Eine Einheit zur Leistungsmessung",
+        "answer_2": "Eine Datenschnittstelle",
+        "answer_3": "Ein Siegel für 'Silent' -PCs",
+        "answer_4": "Eine Router-Einstellung",
+        "right_answer": 2
+    },
+
+    //########################################
+];
