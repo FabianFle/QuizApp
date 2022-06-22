f79e140d0622f23b05813d069aa6a4db36013f1d
FabianFle <fabianflegler1@yahoo.de>
2022-06-22 19:30:26 +0200

    22.06.2022 19:30 - subtleties repaired

--- script.js
+++ script.js
@@ -4,43 +4,38 @@ let currentQuestion = 0;
 
 function initHTML() {
     document.getElementById('quizNumber').innerHTML = questionsHTML.length;
-
     showQuestionHTML()
 }
 
 function initCSS() {
     document.getElementById('quizNumber').innerHTML = questionsCSS.length;
-
     showQuestionCSS()
 }
 
 function initJS() {
     document.getElementById('quizNumber').innerHTML = questionsJS.length;
-
     showQuestionJS()
 }
 
 function initGalaxy() {
     document.getElementById('quizNumber').innerHTML = questionsGalaxy.length;
-
     showQuestionGalaxy()
 }
 
 function initTierwelt() {
     document.getElementById('quizNumber').innerHTML = questionsTierwelt.length;
-
     showQuestionTierwelt()
 }
 
 function initTechnik() {
     document.getElementById('quizNumber').innerHTML = questionsTechnik.length;
-
     showQuestionTechnik()
 }
 
+
+
 function showQuestionHTML() {
     let questionHTML = questionsHTML[currentQuestion];
-
     document.getElementById('questionText').innerHTML = questionHTML['question'];
     document.getElementById('answer_1').innerHTML = questionHTML['answer_1'];
     document.getElementById('answer_2').innerHTML = questionHTML['answer_2'];
@@ -50,7 +45,6 @@ function showQuestionHTML() {
 
 function showQuestionCSS() {
     let questionCSS = questionsCSS[currentQuestion];
-
     document.getElementById('questionText').innerHTML = questionCSS['question'];
     document.getElementById('answer_1').innerHTML = questionCSS['answer_1'];
     document.getElementById('answer_2').innerHTML = questionCSS['answer_2'];
@@ -60,7 +54,6 @@ function showQuestionCSS() {
 
 function showQuestionJS() {
     let questionJS = questionsJS[currentQuestion];
-
     document.getElementById('questionText').innerHTML = questionJS['question'];
     document.getElementById('answer_1').innerHTML = questionJS['answer_1'];
     document.getElementById('answer_2').innerHTML = questionJS['answer_2'];
@@ -70,7 +63,6 @@ function showQuestionJS() {
 
 function showQuestionGalaxy() {
     let questionGalaxy = questionsGalaxy[currentQuestion];
-
     document.getElementById('questionText').innerHTML = questionGalaxy['question'];
     document.getElementById('answer_1').innerHTML = questionGalaxy['answer_1'];
     document.getElementById('answer_2').innerHTML = questionGalaxy['answer_2'];
@@ -80,7 +72,6 @@ function showQuestionGalaxy() {
 
 function showQuestionTierwelt() {
     let questionTierwelt = questionsTierwelt[currentQuestion];
-
     document.getElementById('questionText').innerHTML = questionTierwelt['question'];
     document.getElementById('answer_1').innerHTML = questionTierwelt['answer_1'];
     document.getElementById('answer_2').innerHTML = questionTierwelt['answer_2'];
@@ -90,7 +81,6 @@ function showQuestionTierwelt() {
 
 function showQuestionTechnik() {
     let questionTechnik = questionsTechnik[currentQuestion];
-
     document.getElementById('questionText').innerHTML = questionTechnik['question'];
     document.getElementById('answer_1').innerHTML = questionTechnik['answer_1'];
     document.getElementById('answer_2').innerHTML = questionTechnik['answer_2'];
